0d1e62fcbd857ddd35a398442421e117f5f8956c
lagatita <82121360+LagatitaUwU@users.noreply.github.com>
2022-02-10 20:14:04 -0600

    Se agrega Ruteos de los componentes y barra para navegar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CatalagoAmigosComponent } from './catalago-amigos/catalago-amigos.component';
+import { ComoAdoptarComponent } from './como-adoptar/como-adoptar.component';
+import { ComoDarAdopcionComponent } from './como-dar-adopcion/como-dar-adopcion.component';
+import { AcercaComponent } from './acerca/acerca.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  {path:'CatalagoAmigo',
+   component:CatalagoAmigosComponent
+  },
+  {path:'ComoAdoptar',
+   component:ComoAdoptarComponent
+  },
+  {path:'ComoDarAdopcion',
+   component:ComoDarAdopcionComponent
+  },
+  {path:'Acerca',
+   component:AcercaComponent
+  },
+  {path:'inicio',
+   component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
